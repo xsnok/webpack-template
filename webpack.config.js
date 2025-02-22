94ed59b61b44ac562e01e2dd63430ab560c087c2
Snok <chocowaco152@gmail.noreply.github.com>
2025-02-22 05:51:38 -1000

    Fix webpack config to go to javascript folder

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development",
-  entry: "./src/index.js",
+  entry: "./src/javascript/index.js",
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
